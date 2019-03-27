763a248043c12f0316cd48d94bdccfec884341f1
Sander Cokart <cokart32@gmail.com>
2019-03-27 14:10:15 +0100

    Assignment 2-3 merged

--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -4,11 +4,15 @@ import assignment2.classes.Auto;
 import assignment2.classes.Vervoermiddel;
 
 public class Main {
+
     public static void main(String[] args) {
-        Vervoermiddel vervoermiddel = new Vervoermiddel();
         Auto auto = new Auto();
+        auto.setMerk("Audi");
+        auto.setType("Convertable");
+        auto.setKleur("rood");
         auto.setBrandstof("benzine");
-        auto.setGewicht(1100);
-        System.out.println(auto.berekenWegenbelasting(auto));
+        auto.setGewicht(1998);
+
+        System.out.println("Wegenbelasting is: " + auto.berekenWegenbelasting(auto));
     }
 }
